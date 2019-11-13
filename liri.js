8ce4769d792779ff6f374ba036aa30748f7d1c06
Jonathan Johnson <jraf7@icloud.com>
2019-11-13 17:57:17 -0500

    seatgeek function complete

--- liri.js
+++ liri.js
@@ -1,71 +1,72 @@
-require("dotenv").config();let keys = require("./keys.js");
-let Spotify = require("node-spotify-api")
+require("dotenv").config();
 
+let keys = require("./keys.js");
+let Spotify = require("node-spotify-api")
+let fs = require("fs");
 let axios = require("axios");
 let moment = require("moment");
 
 let spotify = new Spotify(keys.spotify);
-// let seatGeek = new SeatGeek(keys.seatGeek);
-
-
-
 
 let action = process.argv[2];
 let search = process.argv.slice(3).join(" ");
 
-switch (action){
-    case ("concert-this"):
-        goConcert();
-        console.log("Searching for Concerts!")
-        break;
-    case ("spotify-this-song"):
-        goSong();
-        break;
-    case ("movie-this"):
-        goMovie();
-        break;
-    case ("do-what-it-says"):
-        goDo();
-        break;
-    default:
-        return "Invalid Input";
-}
+function goLiri(action, search) {
+    switch (action) {
+        case ("concert-this"):
+            goConcert(search);
+            console.log("Searching for Concerts!")
+            break;
+        case ("spotify-this-song"):
+            goSong();
+            break;
+        case ("movie-this"):
+            goMovie();
+            break;
+        case ("do-what-it-says"):
+            goDo();
+            break;
+        default:
+            return "Invalid Input";
+    }
+};
 
-function goConcert (search) {
+goLiri(action, search);
 
-let URL = "https://api.seatgeek.com/2/events?client_id=MTk0NDI0Nzl8MTU3MzY1MjY5OS44&performers.slug=" + search
+function goConcert(search) {
 
-axios.get(URL)
-.then( function(response){
-    console.log(response.data)
-for (let i = 0; i < response.data.events.length; i++){
-    console.log(`
-    ------------------------------------
-    Performer: 
-    ${response.data.events[i].performer[0].name}
-    Venue: 
-    ${response.data.events[i].venue.name}
-    ${response.data.events[i].venue.city}
-    ${moment(response.data.events[i].datetime_local).format("MM/DD/YYYY")}
-    -------------------------------------
-    `)
-}
+    let URL = `https://api.seatgeek.com/2/events?q=${search}&client_id=${keys.seatGeek.id}`
 
-})
-.catch(function(error) {
-    console.log("An error has occured: " + error);
-})
+    axios.get(URL).then(function (response) {
+        let concert = response.data.events[0];
+        console.log("-----------------------")
+        console.log(`
+Event:
+    ${concert.title}
+Performer: 
+    ${concert.performers[0].name} 
+Venue: 
+    ${concert.venue.name}
+    ${concert.venue.city}
+    ${concert.venue.state}
+    ${moment(concert.datetime_local).format("MM/DD/YYYY")}
+        `)
+        console.log("-----------------------")
+    })
+    .catch(function (error) {
+        console.log("An error has occured: " + error);
+    })
 
 };
 
-function goSong (search) {
-    
-};
+    function goSong(search) {
 
-function goMovie (search) {
-    
-};
+    };
 
-function goDo (search) {
-    
-};
+    function goMovie(search) {
+
+    };
+
+    function goDo(search) {
+
+    };
